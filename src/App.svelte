de1693dbff9596730b1401926d065ac0d62eb2b6
Tonika Valderrama <tokich6@gmail.com>
2020-07-10 23:00:50 -0400

    add email validation

--- src/App.svelte
+++ src/App.svelte
@@ -51,10 +51,9 @@
   let bodyRotationalClass = "";
   let batteryRotationalClass = "";
 
-
   setTimeout(() => {
     showInitialText = true;
-  }, 30000);
+  }, 3000);
 
   function yesClicked() {
     initialPromptHidden = "fadeOut";
@@ -65,18 +64,32 @@
     }, 1000);
   }
 
+  // email validation
+  let formInput;
+
+  const emailLength = val => val.trim().length > 0;
+  
+  const validateEmail = val => {
+    return new RegExp(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ).test(val);
+  };
+  
+  $:validEmail = validateEmail(formInput) && emailLength(formInput);
+
   function submitEmail() {
-    console.log("email submitted");
-    formEnter = "fadeOut";
-    formSubmitted = "fadeIn";
-    setTimeout(() => {
-      showForm = false;
-      showResendOption = true;
-    }, 1000);
-
-    // TODO: EMAIL SUBSRCIPTION
+    if (validEmail) {
+      console.log("email submitted");
+      formEnter = "fadeOut";
+      formSubmitted = "fadeIn";
+      setTimeout(() => {
+        showForm = false;
+        showResendOption = true;
+      }, 1000);
+    } else {
+      alert("please enter a valid email address");
+    }
   }
-
 
   function backToForm() {
     formSubmitted = "fadeOut";
@@ -92,18 +105,17 @@
   }
 
   let position = { x: 0, y: 0 };
-  interact('.draggable').draggable({
-     autoScroll: true,
-     listeners: {
-       move: dragMoveListener
-     }
-  })
-
+  interact(".draggable").draggable({
+    autoScroll: true,
+    listeners: {
+      move: dragMoveListener
+    }
+  });
 
   function dragMoveListener(event) {
     // event.preventDefault();
     var target = event.target;
-   
+
     // keep the dragged position in the data-x/data-y attributes
     var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
     var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
@@ -235,59 +247,57 @@
     width: 16em;
   }
 
-  @media (max-width: 850px){
-      .clouds{
-        margin-bottom: 32%;
-        margin-top: 1rem;
-  
-        }
-      .cloud1{
-        left: 45%;
-        width: 5em;
-      }
-      .cloud2{
-        left: 17%;
-        width: 10em;
-        
-      }
-      .cloud3{
-          left: 2%;
-          width: 6em;
-      }
-      .cloud4{
-        left: 70%;
-        width: 8em;
-      }
-      .head{
-        margin-right: 0rem;
-        margin-left: 0rem;
-      }
+  @media (max-width: 850px) {
+    .clouds {
+      margin-bottom: 32%;
+      margin-top: 1rem;
+    }
+    .cloud1 {
+      left: 45%;
+      width: 5em;
+    }
+    .cloud2 {
+      left: 17%;
+      width: 10em;
+    }
+    .cloud3 {
+      left: 2%;
+      width: 6em;
+    }
+    .cloud4 {
+      left: 70%;
+      width: 8em;
+    }
+    .head {
+      margin-right: 0rem;
+      margin-left: 0rem;
+    }
 
-      .body{
-           margin-right: 0.1rem;
-          margin-left: 0rem;
-      }
-      .right-leg{
-           margin-right: 0.1rem;
-          margin-left: 0rem;
-      }
-      .left-leg{
-           margin-right: 0.1rem;
-          margin-left: 0.1rem;
-      }
-      .right-arm{
-           margin-right: 0.1rem;
-          margin-left: 0.1rem;
-      }
-      .left-arm{
-           margin-right: 0.1rem;
-          margin-left: 0.1rem;
-      }
-      .battery{
-           margin-right: 0.1rem;
-          margin-left: 0.1rem;
-      }
-      /* .diagonal{
+    .body {
+      margin-right: 0.1rem;
+      margin-left: 0rem;
+    }
+    .right-leg {
+      margin-right: 0.1rem;
+      margin-left: 0rem;
+    }
+    .left-leg {
+      margin-right: 0.1rem;
+      margin-left: 0.1rem;
+    }
+    .right-arm {
+      margin-right: 0.1rem;
+      margin-left: 0.1rem;
+    }
+    .left-arm {
+      margin-right: 0.1rem;
+      margin-left: 0.1rem;
+    }
+    .battery {
+      margin-right: 0.1rem;
+      margin-left: 0.1rem;
+    }
+    /* .diagonal{
         transform: rotate(-45deg);
         margin-left: 2.5rem;
         margin-top: .5rem;
@@ -295,48 +305,45 @@
       } */
   }
 
-  @media (max-width: 640px){
-      .clouds{
-        margin-bottom: 29%;
-        margin-top: 1rem;
-  
-        }
-      .cloud1{
-        left: 45%;
-        width: 5em;
-      }
-      .cloud2{
-        left: 17%;
-        width: 7em;
-        
-      }
-      .cloud3{
-          left: 2%;
-          width: 5em;
-      }
-      .cloud4{
-        left: 70%;
-        width: 6em;
-      }
-      .initial-text {
-        overflow-wrap: break-word;
-        word-wrap: break-word;
-        hyphens: auto;
-      }
-     .form-email{
-       margin-left:-2rem;
-     }
-      
+  @media (max-width: 640px) {
+    .clouds {
+      margin-bottom: 29%;
+      margin-top: 1rem;
+    }
+    .cloud1 {
+      left: 45%;
+      width: 5em;
+    }
+    .cloud2 {
+      left: 17%;
+      width: 7em;
+    }
+    .cloud3 {
+      left: 2%;
+      width: 5em;
+    }
+    .cloud4 {
+      left: 70%;
+      width: 6em;
+    }
+    .initial-text {
+      overflow-wrap: break-word;
+      word-wrap: break-word;
+      hyphens: auto;
+    }
+    .form-email {
+      margin-left: -2rem;
+    }
   }
 
-  .flySlow{
-     animation: flyRight 80s linear infinite; 
+  .flySlow {
+    animation: flyRight 80s linear infinite;
   }
 
-  .flyFaster{
-     animation: flyRight 30s linear infinite; 
+  .flyFaster {
+    animation: flyRight 30s linear infinite;
   }
-  .flyRealSlow{
+  .flyRealSlow {
     animation: flyRight 130s linear infinite;
   }
   .flyMed {
@@ -413,11 +420,10 @@
     }
   } */
 
-  .head-drop{
+  .head-drop {
     position: absolute;
     top: 50%;
     left: 50%;
-    
   }
 
   @keyframes fadeIn {
@@ -483,9 +489,8 @@
 <Tailwindcss />
 
 <div id="main-container" class="overflow-hidden">
-  
-  <div class="relative w-screen">
 
+  <div class="relative w-screen">
 
     <div class="clouds relative">
 
@@ -1537,18 +1542,23 @@
             <Button rounded={true} btnText="yes!" on:click={yesClicked} />
           </div>
         </div>
-
       {:else if showForm}
         <div class={`${formEnter} sm:mt-8 form form-email`}>
 
-        <form action="https://formspree.io/xoqkkgzd" method="POST">
-            <Textfield placeHolder="What's your email address?" />
-            <input type="hidden" name="_subject" value="New submission!" />
+          <!-- <form action="https://formspree.io/xoqkkgzd" method="POST"> -->
+          <Textfield
+            placeHolder="What's your email address?"
+            on:input={event => (formInput = event.target.value)} />
+          <input type="hidden" name="_subject" value="New submission!" />
           <span class="mx-2">
+          <!-- {#if validEmail} -->
             <Button rounded={true} btnText="send" on:click={submitEmail} />
+          <!-- {:else}
+            <p>Enter valid email</p> -->
+          <!-- {/if} -->
           </span>
-        </form>
-          
+          <!-- </form> -->
+
         </div>
       {:else if showResendOption}
         <div class={`${formSubmitted} form`}>
@@ -1931,12 +1941,12 @@
     class="-mt-2"
     footerList={footerData}
     on:click={() => (showOverlay = true)} />
-    
-    {#if showOverlay}
-    <Overlay on:click={()=> (showOverlay = false)}>
-     <!-- slot for content, uncomment below to see example-->
-     <!-- <p>I'm a modal</p> -->
+
+  {#if showOverlay}
+    <Overlay on:click={() => (showOverlay = false)}>
+      <!-- slot for content, uncomment below to see example-->
+      <!-- <p>I'm a modal</p> -->
     </Overlay>
-    {/if}
+  {/if}
 
 </div>
--- src/components/Textfield.svelte
+++ src/components/Textfield.svelte
@@ -1,10 +1,13 @@
 <script>
     export let outlined = false;
     export let placeHolder;
+    
+
     let fill = "bg-blue-200 bg-opacity-25 text-white";
     let outline = outlined ? "" : "border-0 focus:outline-none";
     let textfield_template = `mx-3 ${outline} ${fill} py-1 px-2 w-full rounded-full`;
     let focusEffect;
+
     
     function onFocus(){
         focusEffect =  "focus-effect-bottom";
@@ -33,13 +36,17 @@
         color: #ccc;
         font-size: .9rem;
     }
+
+    
 </style>
 
 <div class="inline-block mx-1 text-input" >
     {#if !outlined}
-        <input placeholder="{placeHolder}" on:focus={onFocus} on:blur={onBlur} class="{textfield_template}" type="email" name="email">
+        <input placeholder="{placeHolder}" on:focus={onFocus} on:blur={onBlur} class="{textfield_template}" 
+        type="email" name="email" on:input >
         
     {:else if outlined}
-        <input placeholder="{placeHolder}" on:focus={onFocus} on:blur={onBlur} class="{textfield_template}" type="email" name="email"> 
+        <input placeholder="{placeHolder}" on:focus={onFocus} on:blur={onBlur} class="{textfield_template}"  
+        type="email" name="email" on:input> 
     {/if}
 </div>
